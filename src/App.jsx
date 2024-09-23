9ba5bcd4890ebf703785978009693a14f2856232
Karema <karemahamdy51@gmail.com>
2024-09-23 18:57:02 +0300

    chore: make a base of styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ function App() {
 
 
   return (
-    <>
+    <div className="flex flex-col gap-8 m-8">
+      <div>
+        <p> hellooooooo </p>
+      </div>
     <h1 className="text-3xl font-bold underline text-white">
       Hello world!
     </h1>
-    </>
+    </div>
   )
 }
 
