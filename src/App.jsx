df3600c15e68feed194ff8fd7f8c9603696092c2
Karema <karemahamdy51@gmail.com>
2024-09-23 18:49:42 +0300

    chore: make a base of styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-white">
       Hello world!
     </h1>
     </>
