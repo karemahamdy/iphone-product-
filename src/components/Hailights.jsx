f1156366869ed202055c3265c0a39fb351ad523c
Karema <karemahamdy51@gmail.com>
2024-09-24 23:26:57 +0300

    feat: add animation

--- src/components/Hailights.jsx
+++ src/components/Hailights.jsx
@@ -1,19 +1,26 @@
 import  watchImg from "/assets/images/watch.svg"
 import  rightImg from "/assets/images/right.svg"
-
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 function Hailights() {
+
+  useGSAP(() => {
+    gsap.to('#link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+    gsap.to('#title', { opacity: 1, y: 0, delay: 1 })
+  }, [])
+
   return (
     <section className="mx-24 my-8 mt-5 bg-zinc-950">
       <div className=" flex-box m-7">
-        <h1 className="section-heading">Get the highlights.</h1>
+        <h1 className="section-heading" id="title">Get the highlights.</h1>
 
-        <div className="flex flex-wrap items-end  gap-5">
-          <p className="link">
+        <div className="flex flex-wrap items-end  gap-5" >
+          <p className="link" id="link">
             Watch the film
             <img src={watchImg} alt="watch"  className="ml-2"/>
           </p>
-          <p className="link">
+          <p className="link" id="link">
             Watch the event 
             <img src={rightImg} alt="watch" className="ml-2"/>
           </p>
