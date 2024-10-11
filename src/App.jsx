36cbb3ae61e563c04504aff7948f0284b3d2c658
Karema <karemahamdy51@gmail.com>
2024-10-11 17:47:48 +0300

    build to production

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Nav from "./components/Nav"
 import Hero from "./components/Hero"
 import Hailights from "./components/Hailights"
-import Model from './components/Model'
+// import Model from './components/Model'
 import HowItWorks from './components/Howitworks'
 import Features from './components/Feature'
 import Footer from './components/Footer'
